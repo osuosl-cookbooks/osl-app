7f550310cfbcf902c4d35f357c83eea0b2d65389
armiller <armiller@osuosl.org>
2016-03-23 10:03:01 -0700

    Add some basic tests

--- test/integration/default/serverspec/server_spec.rb
+++ test/integration/default/serverspec/server_spec.rb
@@ -1,3 +1,15 @@
 require 'serverspec'
 
 set :backend, :exec
+
+%w(sqlite-devel libyaml-devel readline-devel zlib-devel libffi-devel
+   openssl-devel automake libtool python git).each do |p|
+  describe package(p) do
+    it { should be_installed }
+  end
+end
+
+describe command('node --version') do
+  its(:exit_status) { should eq 0 }
+  its(:stdout) { should match(/v0\.10\.42/) }
+end
