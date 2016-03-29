c5f30447496299232cea984f1993b9f41154f9a3
armiller <armiller@osuosl.org>
2016-03-29 12:35:34 -0700

    Bump to 1.0

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'chef@osuosl.org'
 license          'apache2'
 description      'Installs/Configures osl-app'
 long_description 'Installs/Configures osl-app'
-version          '0.1.0'
+version          '1.0.0'
 
 supports         'centos', '~> 7.0'
 
@@ -15,4 +15,3 @@ depends          'poise-python'
 depends          'nodejs'
 depends          'sudo'
 depends          'user'
-
