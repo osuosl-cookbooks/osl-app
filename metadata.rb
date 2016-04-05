fa66740d32c6e5028d6b3775e12750498733c02a
armiller <armiller@osuosl.org>
2016-04-04 18:32:53 -0700

    Bump cookbook

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'chef@osuosl.org'
 license          'apache2'
 description      'Installs/Configures osl-app'
 long_description 'Installs/Configures osl-app'
-version          '1.0.0'
+version          '1.0.1'
 
 supports         'centos', '~> 7.0'
 
