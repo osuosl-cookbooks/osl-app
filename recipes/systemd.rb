a8b6c1dee47cf9678855afa0217497b2f90a9d36
armiller <armiller@osuosl.org>
2016-04-28 16:40:25 -0700

    Add systemd service files for openid staging rails app

--- recipes/systemd.rb
+++ recipes/systemd.rb
@@ -15,3 +15,43 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+systemd_service 'openid-staging-unicorn' do
+  description 'openid staging app'
+  after %w(network.target)
+  install do
+    wanted_by 'multi-user.target'
+  end
+  service do
+    type 'forking'
+    user 'openid-staging'
+    environment 'RAILS_ENV' => 'staging'
+    working_directory '/home/openid-staging/current'
+    pid_file '/home/openid-staging/current/tmp/pids/unicorn.pid'
+    exec_start '/home/openid-staging/.rvm/bin/rvm do bundle exec unicorn '\
+    '-c /home/openid-staging/current/config/unicorn/staging.rb -E deployment -D'
+  end
+end
+
+systemd_service 'openid-staging-delayed-job' do
+  description 'openid delayed job'
+  after %w(network.target openid-staging-unicorn.service)
+  wants %w(openid-staging-unicorn.service)
+  install do
+    wanted_by 'multi-user.target'
+  end
+  service do
+    type 'forking'
+    user 'openid-staging'
+    environment 'RAILS_ENV' => 'staging'
+    working_directory '/home/openid-staging/current'
+    exec_start '/home/openid-staging/.rvm/bin/rvm do bundle exec '\
+    'bin/delayed_job -n 2 restart'
+  end
+end
+
+%w(openid-staging-unicorn openid-staging-delayed-job).each do |s|
+  service s do
+    action [:start, :enable]
+  end
+end
