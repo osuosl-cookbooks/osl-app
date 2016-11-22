8f6ed80f7e9fd375d86379427767ba7b75437ee9
Aileen Thai <thai@osuosl.org>
2016-11-22 15:31:58 -0800

    libjpeg-devel --> libjpeg-turbo-devel

--- test/integration/default/serverspec/server_spec.rb
+++ test/integration/default/serverspec/server_spec.rb
@@ -6,7 +6,8 @@ set :path, '/usr/local/bin:$PATH'
 
 %w(sqlite-devel libyaml-devel readline-devel zlib-devel libffi-devel
    openssl-devel automake libtool python git postgresql-devel gdal-python
-   geos-python proj proj-nad freetype-devel libpng-devel).each do |p|
+   geos-python proj proj-nad freetype-devel libjpeg-turbo-devel
+   libpng-devel).each do |p|
   describe package(p) do
     it { should be_installed }
   end
