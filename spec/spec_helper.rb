b78ad29233beed7e68416253e2742184264e16b4
Kerry Vance <vancelot@osuosl.org>
2019-09-12 16:28:16 -0700

    ChefSpec fixes

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,15 +1,13 @@
 require 'chefspec'
 require 'chefspec/berkshelf'
 
-ChefSpec::Coverage.start! { add_filter 'osl-app' }
-
 CENTOS_7 = {
   platform: 'centos',
-  version: '7.4.1708',
+  version: '7',
 }.freeze
 
 RSpec.configure do |config|
-  config.log_level = :fatal
+  config.log_level = :warn
 end
 
 shared_context 'common_stubs' do
