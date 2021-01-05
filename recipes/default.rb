fd18a5e36b801bf4e757586483d6a5cf2fc4ca5e
Ryan <kennedy@osuosl.org>
2021-01-05 15:51:20 -0800

    remove deprecated attributes

--- recipes/default.rb
+++ recipes/default.rb
@@ -15,8 +15,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-node.default['yum']['powertools']['enabled'] = true
-node.default['yum']['powertools']['managed'] = true
 include_recipe 'osl-repos::centos'
 include_recipe 'osl-repos::epel'
 include_recipe 'osl-mysql::client'
